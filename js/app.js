c94851a52fc8deaa9330682dfdde0ac7a77c6b6d
angelo <a@a.a>
2026-05-29 16:20:58 +0200

    added home page with start test button

--- js/app.js
+++ js/app.js
@@ -1,53 +1,67 @@
-const container =
-    document.getElementById("question-container");
 
-// ------ FORMATO A -----------------------------------------------------------------
-questions_8.forEach(question => {
-    container.innerHTML += `
-        <div class="question">
-            <h3>${question.text}</h3>
+
+
+// const container =
+//     document.getElementById("question-container");
+
+// // ------ FORMATO A -----------------------------------------------------------------
+// procesamiento_informacion.forEach(question => {
+//     container.innerHTML += `
+//         <div class="question">
+//             <h3>${question.text}</h3>
             
-            <div id="barra-seleccion">
+//             <div id="barra-seleccion">
 
-                <input
-                    type="range"
-                    id="q-${question.id}"
-                    name="rating"
-                    min="1"
-                    max="5"
-                />
+//                 <input
+//                     type="range"
+//                     id="q-${question.id}"
+//                     name="rating"
+//                     min="1"
+//                     max="5"
+//                 />
 
-                <div id="leyendas">
-                    <span style="width: 20%; text-align: center;">Muy poco</span>
-                    <span style="width: 20%; text-align: center;">Poco</span>
-                    <span style="width: 20%; text-align: center;">Medio</span>
-                    <span style="width: 20%; text-align: center;">Mucho</span>
-                    <span style="width: 20%; text-align: center;">Muchísimo</span>
-                </div>
+//                 <div id="leyendas">
+//                     <span style="width: 20%; text-align: center;">Muy poco</span>
+//                     <span style="width: 20%; text-align: center;">Poco</span>
+//                     <span style="width: 20%; text-align: center;">Medio</span>
+//                     <span style="width: 20%; text-align: center;">Mucho</span>
+//                     <span style="width: 20%; text-align: center;">Muchísimo</span>
+//                 </div>
 
-            </div>
-        </div>
-    `;
-});
+//             </div>
+//         </div>
+//     `;
+// });
 
-// ------ FORMATO B -----------------------------------------------------------------
-questions_2.forEach(question => {
-    if (question.dimension === "situation") {
-        container.innerHTML += `
-            <div class="question">
-                <h3>${question.text}</h3>
+// // ------ FORMATO B -----------------------------------------------------------------
+// questions_2.forEach(question => {
+//     if (question.dimension === "situation") {
+//         container.innerHTML += `
+//             <div class="question">
+//                 <h3>${question.text}</h3>
                 
-                <div id ="box-${question.id}">
-                    <div id="pregunta">
-                        <div class="respuestas-box" name="box-${question.id}">${question.answers[0]}</div>
-                        <div class="respuestas-box" name="box-${question.id}">${question.answers[1]}</div>
-                        <div class="respuestas-box" name="box-${question.id}">${question.answers[2]}</div>
-                        <div class="respuestas-box" name="box-${question.id}">${question.answers[3]}</div>
-                    </div>
-                </div>
+//                 <div id ="box-${question.id}">
+//                     <div id="pregunta">
+//                         <div class="respuestas-box" name="box-${question.id}">${question.answers[0]}</div>
+//                         <div class="respuestas-box" name="box-${question.id}">${question.answers[1]}</div>
+//                         <div class="respuestas-box" name="box-${question.id}">${question.answers[2]}</div>
+//                         <div class="respuestas-box" name="box-${question.id}">${question.answers[3]}</div>
+//                     </div>
+//                 </div>
 
-            </div>
-        `;
+//             </div>
+//         `;
+//     }
+// });
+
+const startTestBtn = document.getElementById("startTestBtn");
+const homePage = document.querySelector(".home");
+const testPage = document.querySelector(".test");
+
+startTestBtn?.addEventListener("click", () => {
+    homePage?.classList.add("hidden");
+    if (testPage) {
+        testPage.classList.remove("hidden");
     }
 });
 
